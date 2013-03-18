a3c971acb1ba564f7e55afd1200b88114313f049
Dmitriy Savchenko <login.was.here@gmail.com>
2013-03-19 01:46:21 +0200

    Added solution for task01

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -2,6 +2,27 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
+(defn- is-link-container? [item]
+  (and (> (count (children item)) 0)
+       (map? (attributes item))
+       (= "r" (:class (attributes item)))))
+
+(defn- get-link-from-container [node]
+  (-> (children node) (first) (attributes) (get :href)))
+
+(defn- process-tree [node elements]
+  (cond
+    (not (vector? node)) elements
+    :else
+      (cond
+        (is-link-container? node) (conj elements (get-link-from-container node))
+        :else
+          (loop [items node
+                 result elements]
+            (cond
+              (empty? items) result
+              :else
+                (recur (next items) (process-tree (first items) result)))))))
 
 (defn get-links []
 " 1) Find all elements containing {:class \"r\"}.
@@ -21,9 +42,7 @@ The link from the example above is 'https://github.com/clojure/clojure'.
 Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
 "
   (let [data (parse "clojure_google.html")]
-    nil))
+    (process-tree data [])))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
-
-
